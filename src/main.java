d1befc0d8c0459e3d05a0e6662ec18ff401e0403
Anastasia Kikot <kikotnastia18@gmail.com>
2021-11-04 23:38:51 +0300

    bonus

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        int ticketPrice = 1000;
+        int mile = 20;
+        int bonus = ticketPrice / mile;
+System.out.println( "Количество бонусных миль " + bonus ) ;
+    }
+}
